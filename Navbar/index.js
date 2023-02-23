877215dff8bea5ab09ac9d28b1d56b7e574721fe
Rajaruban Rajindram <ahyat888@gmail.com>
2023-02-23 15:40:19 +0800

    first commit

--- Navbar/index.js
+++ Navbar/index.js
@@ -2,7 +2,11 @@
 const accordions = document.querySelectorAll(".accordion");
 const listItems = document.querySelectorAll(".main__nav_listItem");
 const activeListItems = document.querySelectorAll(".main__nav_listItem.active");
-const nav = document.querySelector(".navbar-expand-md nav");
+const nav = document.querySelector(".navbar-expand-xl nav");
+const mobileNav = document.querySelector(".navbar-collapse.collapse");
+const navbarToggler = document.querySelector(".navbar-toggler");
+const header = document.querySelector(".navbar-expand-xl");
+const mobileNavPanel = document.querySelector(".navbar-nav");
 
 if (nav.classList.contains("desktop-nav")) {
   for (const li of listItems) {
@@ -15,6 +19,12 @@ if (nav.classList.contains("desktop-nav")) {
 }
 
 // Logic for mobile navbar nested navigation
+navbarToggler.addEventListener("click", () => {
+  header.classList.toggle("active");
+  mobileNav.classList.toggle("open");
+  mobileNavPanel.classList.toggle("d-none");
+});
+
 for (const accordion of accordions) {
   if (nav.classList.contains("desktop-nav")) {
     nav.classList.toggle("desktop-nav");
